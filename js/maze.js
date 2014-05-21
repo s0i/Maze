09753d2c76642dc931820220c310e0a11ac2d356
s0i <harleybenoit@gmail.com>
2014-05-21 10:49:58 -0400

    More improvements

--- js/maze.js
+++ js/maze.js
@@ -1,5 +1,16 @@
 var maze_data;
 var tilesize = 32;
+var usedTile = '';
+
+var TileTypeSprite = {
+    Floor: '' /* Floor sprite path */ ,
+    Wall: '' /* Wall sprite path */
+}
+
+var TileType = {
+    Floor: 'Floor',
+    Wall: 'Wall'
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     $('#about-maze').dialog({
@@ -20,11 +31,12 @@ function AddHandlers() {
     var aboutMaze = document.getElementById('about-maze');
 
     can.addEventListener("click", function(e) {
+        e.preventDefault();
         switch (e.keyCode) {
             case 0:
                 var tile = GetTileInfo(can);
-                PopulateTile(tile.x, tile.y, tile.height, tile.type)
-                Render();
+                PopulateTile(tile.x, tile.y, tile.height, tile.type, usedTile)
+                Render(can);
                 break;
         }
     }, false);
@@ -39,35 +51,24 @@ function AddHandlers() {
     about.addEventListener("click", function() {
         $('#about-maze').dialog('isOpen') ? $('#about-maze').dialog('close') : $('#about-maze').dialog('open');
     }, false);
-
-    $("#about-maze").bind("clickoutside", function(event) {
-        $(this).hide();
-    });
 }
 
 function RemoveTile(x, y, array) {
-    if (typeof array[x][y] === undefined) return false;
-    else {
-        array[x][y] = null;
-        return true;
-    }
+    typeof array[x][y] === undefined || null ? array[x][y] = new Tile() : array[x][y] = new Tile();
 }
 
 function GetTileInfo(canvas) {
-    /* Get information of the tile:
-		- Height
-		- Type
-	*/
     var coords = GetMousePosition(canvas);
+    var tile = [maze_data[x][y].type, maze_data[x][y].height];
     return {
         x: coords.x,
         y: coords.y,
-        height: 20,
-        type: 'Wall'
+        height: tile[1],
+        type: tile[0]
     }
 }
 
-function GetMousePosition(canvas) {
+function GetMousePosition(canvas, event) {
     var rect = canvas.getBoundingClientRect();
 
     var x = Math.floor((event.clientX - rect.left) / tilesize);
@@ -79,18 +80,13 @@ function GetMousePosition(canvas) {
     }
 }
 
-function ClickHandler(canvas, event) {
-    var coords = GetMousePosition(canvas, event)
-    return coords
-}
-
 function CreateArray(width, height, tilesize) {
     var array = [];
 
     for (var x = 0; x < (width / tilesize); x++) {
         array[x] = [];
         for (var y = 0; y < (height / tilesize); y++) {
-            array[x][y] = null;
+            array[x][y] = new Tile();
         }
     }
     return array;
@@ -109,6 +105,17 @@ function PopulateTile(x, y, h, t) {
     maze_data[x][y] = temp;
 }
 
-function Render() {
+function Render(canvas) {
+    var c = canvas.getContext('2d');
+
+    for (var tile in maze_data) {
+        if (tile === undefined || null) {
+            tile.type = TileType.Floor;
+            var img = TileTypeSprite.Floor;
+        } else {
+            var img = tile.type;
+            c.draw(img, tile.x, tile.y);
+        }
+    }
     return true;
 }
